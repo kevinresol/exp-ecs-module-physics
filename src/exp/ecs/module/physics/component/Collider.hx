166eea438684337af02a3f622d67a82c66da6d08
Kevin Leung <kevinresol@gmail.com>
2020-10-23 16:08:46 +0800

    Support collider mask

--- src/exp/ecs/module/physics/component/Collider.hx
+++ src/exp/ecs/module/physics/component/Collider.hx
@@ -1,9 +1,15 @@
 package exp.ecs.module.physics.component;
 
 class Collider implements Component {
-	public final with:Array<Int>;
+	public final hits:Array<Int>;
+	public final layers:Int; // bitmask
+	public final targets:Int; // bitmask
 
 	public function clone() {
-		return new Collider(with.copy());
+		return new Collider(hits.copy(), layers, targets);
+	}
+
+	public inline function canCollideWith(other:Collider) {
+		return targets & other.layers > 0;
 	}
 }
